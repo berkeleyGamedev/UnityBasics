ac6fa13af708c5f1ac399647f2d8a6bb0649b530
shibojiang654 <jiangshibo132@gmail.com>
2024-09-03 18:52:10 -0700

    Updated to 2022.3.28f1

---
+++ Documents/test/UnityBasics-master/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    //SerializeField allows private variables to be seen and changed in the inspector
+    [SerializeField] private float speed = 1.5f;
+    [SerializeField] private float rotationSpeed = 2;
+
+    private float maxVelocity = 3;
+    
+    void Start()
+    {
+        //Get a reference to the attached RigidBody2D component
+        rb = GetComponent<Rigidbody2D>();
+    }
+    
+    void Update()
+    {
+        //Get user inputs
+        float xAxis = Input.GetAxis("Horizontal");
+        Rotate(xAxis * -rotationSpeed);
+
+        if (Input.GetKey(KeyCode.Space)) {
+            ThrustForward();
+        }
+
+        ClampVelocity();
+    }
+
+    //Clamp velocity according to max velocity
+    private void ClampVelocity() {
+        float x = Mathf.Clamp(rb.velocity.x, -maxVelocity, maxVelocity);
+        float y = Mathf.Clamp(rb.velocity.y, -maxVelocity, maxVelocity);
+
+        rb.velocity = new Vector2(x, y);
+    }
+
+    //Apply forward force
+    private void ThrustForward() {
+        Vector2 force = transform.up * speed;
+        rb.AddForce(force);
+    }
+
+    //Rotate the object
+    private void Rotate(float amount) {
+        transform.Rotate(0, 0, amount);
+    }
+}
---
+++ Documents/test/UnityBasics-master/Assets/Scripts/UIScript.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIScript : MonoBehaviour
+{
+    public Text scoreText;
+    public Text healthText;
+
+    public Text gameOverText;
+    private bool gameOver;
+
+    [SerializeField] private int scoreToWin;
+    private int score = 0;
+
+    private PlayerHealth player;
+
+    private void Start() {
+        player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerHealth>();
+
+        UpdateUI();
+    }
+
+    private void Update() {
+        UpdateUI();
+    }
+
+    private void UpdateUI() {
+        if (gameOver) {
+            return;
+        }
+
+        scoreText.text = "Score: " + score;
+        healthText.text = "Health: " + player.currentHealth;
+
+        if (player.currentHealth <= 0) {
+            gameOver = true;
+            gameOverText.text = "You lost!";
+        }
+    }
+
+    public void IncreaseScore() {
+        score++;
+
+        if (score >= scoreToWin && !gameOver) {
+            gameOver = true;
+            gameOverText.text = "You won!";
+        }
+    }
+
+}
