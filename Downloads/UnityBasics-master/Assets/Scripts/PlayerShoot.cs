f8cb242c89eebe805200af7b0f02f6b945038e18
shibojiang654 <jiangshibo132@gmail.com>
2024-09-03 19:17:09 -0700

    Updated to 2022.3.28f1

---
+++ Downloads/UnityBasics-master/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject laserPrefab;
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.LeftShift)) {
+            Shoot();
+        }
+    }
+
+    private void Shoot() {
+        GameObject bullet = Instantiate(laserPrefab, transform.position, transform.parent.rotation);
+        bullet.GetComponent<Rigidbody2D>().AddForce(transform.up * 320);
+    }
+}
---
+++ Downloads/UnityBasics-master/Assets/Scripts/ScreenBounds.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenBounds : MonoBehaviour
+{
+    private Camera cam;
+    private float wrapMargin = 5f;
+
+    private void Awake() {
+        cam = GameObject.Find("Main Camera").GetComponent<Camera>();
+    }
+
+    private void Update() {
+        Vector2 screenPosition = cam.WorldToScreenPoint(transform.position);
+
+        if(screenPosition.x < 0)
+        {
+            screenPosition.x = Screen.width - wrapMargin;
+        }
+
+        if(screenPosition.x > Screen.width)
+        {
+            screenPosition.x = wrapMargin;
+        }
+
+        if (screenPosition.y < 0)
+        {
+            screenPosition.y = Screen.height - wrapMargin;
+        }
+
+        if (screenPosition.y > Screen.height)
+        {
+            screenPosition.y = wrapMargin;
+        }
+
+        transform.position = (Vector2) cam.ScreenToWorldPoint(screenPosition);
+    }
+}
